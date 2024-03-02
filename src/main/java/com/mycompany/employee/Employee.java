60cc29a97c4947b2491e77de36829f50785933ae
RafaFernandez92 <rafa.8nov92@gmail.com>
2024-03-02 13:07:36 +0000

    email validation, return values, add the employees

--- src/main/java/com/mycompany/employee/Employee.java
+++ src/main/java/com/mycompany/employee/Employee.java
@@ -38,12 +38,37 @@ public class Employee {
         return email;
     }
     public int getEmpNum(){
-        return nextEmpNum;
+        return empNum;
     }
     
+    public void setEmail(String email) {
+        if (email.length() > 3) {
+            this.email = email;
+        } else {
+            System.out.println("Email must be at least 4 characters long.");
+        }
     
-    
-    public static void main(String[] args) {
-    
+    public int getNextEmpNum(){
+        return nextEmpNum;
+    }   
+public class EmployeeTest {
+    public void main(String[] args) {
+        Employee[] projectGroup = new Employee[3];
+
+        // Create 3 Employee objects
+        projectGroup[0] = new Employee("John Bloggs", "jb@bmail.com");
+        projectGroup[1] = new Employee("Ann Banana", "ab@bmail.com");
+        projectGroup[2] = new Employee("Tom Thumb", "tt@bmail.com");
+
+        // Print the value of nextEmpNum
+        System.out.println("Next employee number: " + Employee.getNextEmpNum());
+
+        // Search and display names of employees with empNum above a certain value (m)
+        int m = 2; // Example value
+        for (Employee emp : projectGroup) {
+            if (emp.getEmpNum() > m) {
+                System.out.println("Employee name: " + emp.getName());
+            }
+        }
     }
 }
