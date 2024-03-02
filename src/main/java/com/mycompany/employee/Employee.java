b90601bf9ab105192f844274bea8aedb4c4de3c8
RafaFernandez92 <rafa.8nov92@gmail.com>
2024-03-02 11:21:50 +0000

    Setting constructors and values

--- src/main/java/com/mycompany/employee/Employee.java
+++ src/main/java/com/mycompany/employee/Employee.java
@@ -10,8 +10,22 @@ package com.mycompany.employee;
  * @author User
  */
 public class Employee {
-
+    String name;
+    String email;
+    int empNum;
+    
+    public nameEmail(String nameDef, String emailDef){
+        name = nameDef;
+        email = emailDef;
+        System.out.println(name + email);
+    }
+    
+    public employeeNum(int Num){
+        empNum = Num;
+        System.out.println(empNum);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        nameEmail first = new nameEmail("Rafael", "Def@defau.com");
+        System.out.println(first.name + first.email);
     }
 }
